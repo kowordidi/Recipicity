f378fe20cd07c54ea6e1f86c346b9eb5323fef19
kowordidi <891olivermoran@gmail.com>
2023-09-07 17:42:51 +0200

    removed empty classes, except recipe added dat folder and first instructions_file

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 class Recipe:
-    pass
-class Ingredient:
-    pass
-class Dish:
-    pass
+    def __init__(self, name, instructions_file, ingredients):
+        self.name = name
+        self.instructions_file = instructions_file
+        self.ingredients = ingredients
+
+    def get_instructions(self):
+        try:
+            with open(self.instructions_file, 'r') as file:
+                return file.read()
+        except FileNotFoundError:
+            return "Instructions not found"
+
+    def set_instructions(self, new_instructions):
+        self.instructions = new_instructions
+
+    def get_ingredients(self):
+        return self.ingredients
+
+    def add_ingredient(self, new_ingredient, new_amount="1 unit"):
+        self.instructions[new_ingredient] = new_amount
+
